908c86d29bc3dd63469a292187c8c44d75c0fdff
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2024-10-02 20:45:06 -0300

    Event insert

--- src/main/java/com/devsuperior/demo/controllers/EventController.java
+++ src/main/java/com/devsuperior/demo/controllers/EventController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/events")
@@ -20,5 +22,13 @@ public class EventController {
     @GetMapping
     public ResponseEntity<Page<EventDTO>> findAllPaged(Pageable pageable) {
         return ResponseEntity.ok(service.findAllPaged(pageable));
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+    @PostMapping
+    public ResponseEntity<EventDTO> insert(@RequestBody EventDTO dto) {
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(service.insert(dto));
     }
 }
--- src/main/java/com/devsuperior/demo/services/EventService.java
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -1,6 +1,8 @@
 package com.devsuperior.demo.services;
 
 import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.entities.Event;
 import com.devsuperior.demo.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -15,5 +17,15 @@ public class EventService {
 
     public Page<EventDTO> findAllPaged(Pageable pageable) {
         return repository.findAll(pageable).map(EventDTO::new);
+    }
+
+    public EventDTO insert(EventDTO dto) {
+        Event entity = new Event();
+        entity.setName(dto.getName());
+        entity.setDate(dto.getDate());
+        entity.setUrl(dto.getUrl());
+        entity.setCity(new City(dto.getCityId(), null));
+        entity = repository.save(entity);
+        return new EventDTO(entity);
     }
 }
