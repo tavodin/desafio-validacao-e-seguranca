c8835a4698e9846727dc0ff391ecccc5deb65c3d
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2024-10-02 20:38:47 -0300

    EventService, EventController, findAllPaged

---
+++ src/main/java/com/devsuperior/demo/controllers/EventController.java
@@ -0,0 +1,24 @@
+package com.devsuperior.demo.controllers;
+
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/events")
+public class EventController {
+
+    @Autowired
+    private EventService service;
+
+    @GetMapping
+    public ResponseEntity<Page<EventDTO>> findAllPaged(Pageable pageable) {
+        return ResponseEntity.ok(service.findAllPaged(pageable));
+    }
+}
---
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -0,0 +1,19 @@
+package com.devsuperior.demo.services;
+
+import com.devsuperior.demo.dto.EventDTO;
+import com.devsuperior.demo.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    public Page<EventDTO> findAllPaged(Pageable pageable) {
+        return repository.findAll(pageable).map(EventDTO::new);
+    }
+}
